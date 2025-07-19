bdba517e61b58f4fe94433ab342cbcf1192d7772
Dev envolvente <97799788+DevEnvolvente0190@users.noreply.github.com>
2025-07-19 16:13:15 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,23 @@
-const tenis1 = document.getElementById('tenis-1');
-const tenis2 = document.getElementById('tenis-2');
-const setaRight = document.getElementById('right');
-const setaLeft = document.getElementById('left');
+const tenis1 = document.getElementById('tenis1');
+const tenis2 = document.getElementById('tenis2');
+
+const arrowLeft = document.getElementById('left');
+const arrowRight = document.getElementById('right');
+
+//Desativa seta de esquerda no inicio
+arrowLeft.classList.add('disable');
 
 setaLeft.addEventListener('click', () => {
     tenis1.style.left = "650px";
     tenis2.style.left ="-50px";
+
+    arrowRight.classList.add('disable');
+    arrowLeft.classList.remove('disable')
 })
 setaRight.addEventListener('click', () => {
     tenis1.style.left = "-50px";
     tenis2.style.left = "-650px";
+
+    arrowLeft.classList.add('disable')
+    arrowRight.classList.remove('disable');
 })
